1e5c14fceff14f8e5beffc7d3049aee59dc73fde
rakesh <modish.jay90@gmail.com>
2024-10-07 10:41:41 +0530

    Initial Commit

---
+++ weather-app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather-app
+//
+//  Created by Rakesh on 07/10/24.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ weather-appTests/weather_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appTests.swift
+//  weather-appTests
+//
+//  Created by Rakesh on 07/10/24.
+//
+
+import Testing
+@testable import weather_app
+
+struct weather_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
